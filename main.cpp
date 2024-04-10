836ff934e25e4a4c9e900208225cc8289d9b2ef4
sadam <sadam@sogang.ac.kr>
2024-04-10 16:26:11 +0900

    main space cleanup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "Stack.h"
 
-void prnMenu(){
+void prnMenu() {
 	cout<<"*******************************************"<<endl;
 	cout<<"* 1. 삽입    2. 삭제    3. 출력   4. 종료 *"<<endl;
 	cout<<"*******************************************"<<endl;
@@ -9,57 +9,58 @@ void prnMenu(){
 	cout<<"원하시는 메뉴를 골라주세요: ";
 }
 
-int main(){
+int main() {
 	    int mode, selectNumber, tmpItem;
 		LinkedList<int> *p;
 		bool flag = false;
-
+		
 		cout<<"자료구조 선택(1: Stack, Other: Linked List): ";
 		cin>>mode;
-						    
-		if(mode == 1)
+		
+		if (mode == 1)
 			p = new Stack<int>();    // 정수를 저장하는 스택
-			
+		
 		else
 			p = new LinkedList<int>();
-
-
-		do{
+		
+		do {
 			prnMenu();
 			cin>>selectNumber;
 			
-			switch(selectNumber){ 
+			switch(selectNumber) {
 				case 1:
 					cout<<"원하시는 값을 입력해주세요: ";
-					cin>>tmpItem;    p->Insert(tmpItem);
+					cin>>tmpItem;
+					p->Insert(tmpItem);
 					cout<<tmpItem<<"가 삽입되었습니다."<<endl;
 					break;
-					
+				
 				case 2:
-					if(p->Delete(tmpItem)==true)
+					if (p->Delete(tmpItem) == true)
 						cout<<tmpItem<<"가 삭제되었습니다."<<endl;
-						
-					else cout<<"비어있습니다. 삭제 실패"<<endl;
-					break;
 					
+					else
+						cout<<"비어있습니다. 삭제 실패"<<endl;
+					break;
+				
 				case 3:
 					cout<<"크기: "<<p->GetSize()<<endl;
 					p->Print();
 					break;
-					
+				
 				case 4:
-					flag = true;     break;
-					
+					flag = true;
+					break;
+				
 				default:
 					cout<<"잘못 입력하셨습니다."<<endl;
 					break;
-			
 			}
 			
-			if(flag) break;
+			if(flag)
+				break;
 			
 		} while(1);
 		
 		return 0;
 }
-
