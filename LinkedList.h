2fab9480384a860762cc3f244172156168149f65
sadam <sadam@sogang.ac.kr>
2024-04-10 16:29:04 +0900

    Linkedlist space cleanup

--- LinkedList.h
+++ LinkedList.h
@@ -6,35 +6,35 @@ using namespace std;
 
 //LinkedList Node
 template <typename T>
-class Node{
+class Node {
 	public:
 		//데이터를 저장할 변수
 		T data;
 		//노드구조체 이용; 다음노드의 주소를 저장할 포인터
 		Node *link;
 
-		Node(T element){
-		  data = element;
-		  link = 0;
+		Node(T element) {
+			data = element;
+			link = 0;
 		}
-	};
+};
 
 template <typename T>
 //LinkedList Class
-class LinkedList{
+class LinkedList {
 	protected:
 		//첫번째 노드의 주소를 저장할 포인터
 		Node<T> *first;
 		int current_size;
 	public:
 		//생성자, 초기화 
-		LinkedList(){
+		LinkedList() {
 			first = 0;
 			current_size = 0;
 		};
 
 		//노드 개수를 리턴
-		int GetSize(){
+		int GetSize() {
 			return current_size;
 		};
 
@@ -51,7 +51,7 @@ class LinkedList{
 
 //새 노드를 맨 앞에 붙이고 값을 넣음
 template <typename T>
-void LinkedList<T>::Insert(T element){
+void LinkedList<T>::Insert(T element) {
 	//노드 생성
 	Node<T> *newnode = new Node<T>(element);
 
@@ -64,7 +64,7 @@ void LinkedList<T>::Insert(T element){
 
 //마지막 노드의 값을 리턴하면서 메모리에서 할당 해제
 template <typename T>
-bool LinkedList<T>::Delete(T &element){
+bool LinkedList<T>::Delete(T &element) {
 
 	if (first == 0)
 		return false;
@@ -73,8 +73,8 @@ bool LinkedList<T>::Delete(T &element){
 	Node<T> *previous = 0;
 	
 	//마지막 노드까지 찾아가는 반복문
-	while(1){
-		if (current->link == 0){  //마지막 노드를 찾는것
+	while(1) {
+		if (current->link == 0) {  //마지막 노드를 찾는것
 			if (previous)
 				previous -> link = current -> link;
 			else
@@ -93,17 +93,17 @@ bool LinkedList<T>::Delete(T &element){
 
 //리스트를 출력하는 Print 함수
 template <typename T>
-void LinkedList<T>::Print(){
+void LinkedList<T>::Print() {
 	Node<T> *i;
 	int index = 1;
 
 	if (current_size != 0){
-		for( i = first; i != NULL; i=i->link){
+		for(i = first; i != NULL; i = i -> link){
 			if (index == current_size){
 				cout << "[" << index << "|";
 				// original line
 				// cout << i -> data <<"]->";
-				cout << i -> data <<"]";
+				cout << i -> data << "]";
 			}
 			
 			else {
